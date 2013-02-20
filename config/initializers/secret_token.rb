aeef5dd50544a84cde2c11a272c7671d38eb0282
Hector Garcia <hectorgarcia@gmail.com>
2013-02-20 15:24:30 +0900

    Clean Rails app start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSaasBuilder::Application.config.secret_token = 'd6cb8ed0042107a042eb7cd2cdf4e078be93b37cb994ec4b611dec0e6b4e0a8038517d99b9c6cc04b490f01f3ad24567e01f60aa03cc0236281e482c123085f6'
